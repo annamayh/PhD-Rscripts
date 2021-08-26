917f7696b95e5d6ae527b7c70a2f8a5e182547f5
annamayh <anna.hewett28@outlook.com>
2021-08-26 13:04:22 +0100

    getting hotspots

--- Haplotypes/iHS for simulated data.R
+++ Haplotypes/iHS for simulated data.R
@@ -33,15 +33,60 @@ for(k in 1:27) {
   
 }  
 
-names(scan_output)<-paste0("Sim_num",1:21) #naming the list elements based on the simulation number 
+#save(scan_output, file="iHS_simulations_scan_output.Rdata")
+#load("Haplotype_diversity/iHS_simulations_scan_output.Rdata")
+
+names(scan_output)<-paste0("Sim_num",1:27) #naming the list elements based on the simulation number 
 
 scan_output["Sim_num20"]<-NULL
 scan_output["Sim_num22"]<-NULL #remove the simulations that didnt work on dud machine
 scan_output["Sim_num25"]<-NULL
-scan_output["Sim_num26"]<-NULL ## this sometimes doesnt work??
+scan_output["Sim_num26"]<-NULL 
 
 
 
 iHS_list<-map(scan_output, ihh2ihs)
 
 
+## read in locations of hotspots for each simulation 
+
+ROH_hotspots_list<-list()
+
+for(l in 1:27) {
+  if (l==20) next ## for failed simulations on dud machine
+  if (l==22) next
+  if (l==25) next
+  if (l==26) next
+
+  ROH_hotspots_list[[l]]<-read.table(paste0("Slim/Ash_server_output/neutral/hom summary files/ROH_out",l,".hom.summary"),header = TRUE, stringsAsFactors=FALSE)%>%
+    select(BP,UNAFF)%>%mutate(pall=UNAFF/100)%>%setNames(.,c("POSITION","UNAFF","pall"))
+  num[l]<-as.numeric(quantile(ROH_hotspots_list[[l]]$pall, c(.99)))
+  ROH_hotspots_list[[l]]$hotspot<-NA
+  ROH_hotspots_list[[l]]$hotspot<-ifelse(ROH_hotspots_list[[l]]$pall>num[l], "yes", "no")
+} 
+  
+names(ROH_hotspots_list)<-paste0("Sim_num",1:27) #naming the list elements based on the simulation number 
+
+ROH_hotspots_list["Sim_num20"]<-NULL
+ROH_hotspots_list["Sim_num22"]<-NULL #remove the simulations that didnt work on dud machine
+ROH_hotspots_list["Sim_num25"]<-NULL
+ROH_hotspots_list["Sim_num26"]<-NULL 
+
+
+
+show_hotspots<-function(sim){
+  num<-as.numeric(quantile(sim$pall, c(.99)))
+  sim$hotspot<-NA
+  sim$hotspot<-ifelse(sim$pall>num, "yes", "no")
+  
+}
+
+
+ROH_hotspots_list_y<-map(ROH_hotspots_list, show_hotspots)
+
+testing<-as.data.frame(ROH_hotspots_list["Sim_num1"])
+
+for (line in 1:nrow(testing["Sim_num1"]))
+num<-as.numeric(quantile(testing[["Sim_num1"]]$pall, c(.99)))
+testing$hotspot<-NA
+testing$hotspot<-ifelse(sim$pall>num, "yes", "no")
