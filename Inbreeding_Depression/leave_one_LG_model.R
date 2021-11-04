d44197d2e70a94517abe0a1e95e8a628f95dc9c7
annamayh <anna.hewett28@outlook.com>
2021-11-04 17:48:44 +0000

    weighted for chr length

--- Inbreeding_Depression/leave_one_LG_model.R
+++ Inbreeding_Depression/leave_one_LG_model.R
@@ -40,7 +40,7 @@ ped[is.na(ped)] <- 0
 ainv <- ainverse(ped)
 
 ################################################
-### read in ROH file to work out FROH per chr ###
+### read in ROH file to work out LG FROH ###
 #################################################
 setwd("H:/")
 
@@ -67,66 +67,42 @@ froh_per_chr<-join(KB_perLG,deersum)%>% mutate(chr_froh=KB_chr/chr_length_KB)%>%
 
 colnames(froh_per_chr) <- c("Code", paste0("FROH_chr", 1:33))
 
-################################################################
-##### Make list of chromosomal FROH plus FROH -focal chr ######
-###### to use in models ######################################
-##############################################################
+
+########
+
+
+
+
 
 for_models<-list()
 for (k in 1:33){
+
   
-  for_models[[k]]<-froh_per_chr
-  for_models[[k]]$froh_minus_focal<-NA
+Focal_LG_FROH<-FROH%>%filter(CHR==k)%>%dplyr::group_by(Code, CHR)%>%
+  dplyr::summarise(KB_chr=sum(KB))%>%
+  ungroup %>% 
+  complete(Code, CHR, fill = list(KB_chr = 0)) %>% join(deersum)%>%
+  mutate(focal_chr_froh=KB_chr/chr_length_KB)%>%
+  select(Code, focal_chr_froh)
+
+
+deersum_minus_focal<-deersum%>%filter(CHR!=k)%>%filter(CHR!=34)
+rest_genome_length<-sum(deersum_minus_focal$chr_length_KB)
+
+Rest_FROH<-FROH%>%filter(CHR!=k)%>%dplyr::group_by(Code)%>%
+  dplyr::summarise(KB_chr=sum(KB))%>%
+  ungroup %>% 
+  mutate(Rest_chr_froh=KB_chr/rest_genome_length)%>%
+  select(-KB_chr)
   
-  col_end_chunk_no1<-k
-  col_start_chunk_no2<-k+2
- 
-  if(k==1){
-  for_models[[k]]$froh_minus_focal<-rowSums(for_models[[k]][(3:34)])# for chr 1 only need one block to sum
-  } 
-  else if(k==33){
-    for_models[[k]]$froh_minus_focal<-rowSums(for_models[[k]][(2:33)])# for chr 33 again only need one block to sum
-  } 
-  else{
-      for_models[[k]]$froh_minus_focal<-rowSums(for_models[[k]][c(2:col_end_chunk_no1,col_start_chunk_no2:34)])
-    # for all the rest sum across rows leaving out the focal chromosome 
-    }
-    
-  for_models[[k]]<-mutate(for_models[[k]],froh_minus_focal_div=froh_minus_focal/32 )#Divide by total number of chr in rest of FROH 
-  for_models[[k]]<-dplyr::select(for_models[[k]], Code, froh_minus_focal_div, paste0("FROH_chr", k))#just keeping inportant columns 
-  names(for_models[[k]])[names(for_models[[k]])== paste0("FROH_chr", k)]<-"Focal_FROH"
-  }
+for_models[[k]]<-join(Focal_LG_FROH,Rest_FROH, type="right")%>%
+  mutate_all(~replace(., is.na(.), 0))#replaces NAs with ) using dplyr
 
-
-
-
-
+} 
 
 ###############################################################################################
 ######## NOW  SET THINGS UP TO RUN MODEL #####################################################
 #############################################################################################
-
-Run_model_all_LG<- function(LG){
-
-Birth_wt_df<-birth_wt%>%join(life)%>%join(mum_stat)%>%join(LG)%>%na.omit()
-
-Birth_wt_df$Sex <- factor(Birth_wt_df$Sex)
-Birth_wt_df$Code <- factor(Birth_wt_df$Code)
-Birth_wt_df$MotherStatus <- factor(Birth_wt_df$MotherStatus)
-Birth_wt_df$MumCode <- factor(Birth_wt_df$MumCode)
-
-
-chr_model_with_ped <- asreml(fixed= CaptureWt ~ 1 + Sex + AgeHrs + froh_minus_focal_div + Focal_FROH + MotherStatus, 
-                    random= ~ vm(Code,ainv)+BirthYear +MumCode, 
-                    residual= ~ idv(units),
-                    data=Birth_wt_df)
-
-
-
-}
-
-
-
 
 for (s in 1:33) {
   
@@ -140,7 +116,7 @@ for (s in 1:33) {
   Birth_wt_df$MumCode <- factor(Birth_wt_df$MumCode)
 
 
-  chr_model_with_ped <- asreml(fixed= CaptureWt ~ 1 + Sex + AgeHrs + froh_minus_focal_div + Focal_FROH + MotherStatus, 
+  chr_model_with_ped <- asreml(fixed= CaptureWt ~ 1 + Sex + AgeHrs + Rest_chr_froh + focal_chr_froh + MotherStatus, 
                              random= ~ vm(Code,ainv)+BirthYear +MumCode, 
                              residual= ~ idv(units),
                              data=Birth_wt_df)
@@ -149,43 +125,5 @@ for (s in 1:33) {
   qgm_summary(chr_model_with_ped)
 
 }
-## this works if i then run each line of the function through??
-#LG<-for_models[[1]]
-#qgm_summary(chr_model_with_ped)
-
-## but then this doesnt??????
-#test<-Run_model_all_LG(LG)
-#qgm_summary(test)
-
-#f=1
-
-#for (f in 1:2){
- # LG_list<-for_models[[f]]
-  #output_model<-Run_model_all_LG(LG_list)
-  #print(paste0("QGM SUMMARY FOR LG", f))
-  #qgm_summary(output_model)
-  
-#}
-
-#model_output<-map(for_models,Run_model_all_LG)
 
 
-#j=1
-
-#for (j in 1:2){
-
-  #model_out<-Run_model_all_LG(for_models[j])
-  #print(paste0("QGM SUMMARY FOR LG", j))
-  #qgm_summary(model_out)
-
-#}
-
-## maybe do no ped too?? 
-
-#chr_model_NO_ped <- asreml(fixed= CaptureWt ~ 1 + Sex + AgeHrs + froh_minus_focal_div + Focal_LG_FROH + MotherStatus, 
- #                          random= ~ BirthYear +MumCode, 
-  #                         residual= ~ idv(units),
-   #                        data=Birth_wt_df)
-
-
-#qgm_summary(chr_model_NO_ped)
