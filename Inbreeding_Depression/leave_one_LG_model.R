652fa59263d2c5eba906b01df7fe77922872919d
annamayh <anna.hewett28@outlook.com>
2021-11-11 16:24:08 +0000

    using map

--- Inbreeding_Depression/leave_one_LG_model.R
+++ Inbreeding_Depression/leave_one_LG_model.R
@@ -71,32 +71,29 @@ colnames(froh_per_chr) <- c("Code", paste0("FROH_chr", 1:33))
 ########
 
 
-
-
-
 for_models<-list()
 for (k in 1:33){
 
   
-Focal_LG_FROH<-FROH%>%filter(CHR==k)%>%dplyr::group_by(Code, CHR)%>%
+Focal_LG_FROH<-FROH%>%filter(CHR==k)%>%dplyr::group_by(Code, CHR)%>% #filter for focal chr and grouping by id and chr
+  dplyr::summarise(KB_chr=sum(KB))%>% #getting total KB per chr per id
+  ungroup %>% 
+  complete(Code, CHR, fill = list(KB_chr = 0)) %>% join(deersum)%>% #joining to total length of chr
+  mutate(focal_chr_froh=KB_chr/chr_length_KB)%>% # working out froh for focal chr 
+  select(Code, focal_chr_froh)#selecting important columns
+
+
+deersum_minus_focal<-deersum%>%filter(CHR!=k)%>%filter(CHR!=34) #getting length of chr  minus focal chr
+rest_genome_length<-sum(deersum_minus_focal$chr_length_KB) #adding all chr together to get length of genome minus focal
+
+Rest_FROH<-FROH%>%filter(CHR!=k)%>%dplyr::group_by(Code)%>%# getting total KB in ROH for rest of genome
   dplyr::summarise(KB_chr=sum(KB))%>%
   ungroup %>% 
-  complete(Code, CHR, fill = list(KB_chr = 0)) %>% join(deersum)%>%
-  mutate(focal_chr_froh=KB_chr/chr_length_KB)%>%
-  select(Code, focal_chr_froh)
-
-
-deersum_minus_focal<-deersum%>%filter(CHR!=k)%>%filter(CHR!=34)
-rest_genome_length<-sum(deersum_minus_focal$chr_length_KB)
-
-Rest_FROH<-FROH%>%filter(CHR!=k)%>%dplyr::group_by(Code)%>%
-  dplyr::summarise(KB_chr=sum(KB))%>%
-  ungroup %>% 
-  mutate(Rest_chr_froh=KB_chr/rest_genome_length)%>%
+  mutate(Rest_chr_froh=KB_chr/rest_genome_length)%>% #Kb in ROH minus focal/ genome length minus focal 
   select(-KB_chr)
   
 for_models[[k]]<-join(Focal_LG_FROH,Rest_FROH, type="right")%>%
-  mutate_all(~replace(., is.na(.), 0))#replaces NAs with ) using dplyr
+  mutate_all(~replace(., is.na(.), 0))#replaces NAs with 0 using dplyr
 
 } 
 
@@ -104,12 +101,10 @@ for_models[[k]]<-join(Focal_LG_FROH,Rest_FROH, type="right")%>%
 ######## NOW  SET THINGS UP TO RUN MODEL #####################################################
 #############################################################################################
 
-for (s in 1:33) {
-  
-  LG<-for_models[[s]]
+run_model<-function(focal_LG_df){
 
-  Birth_wt_df<-birth_wt%>%join(life)%>%join(mum_stat)%>%join(LG)%>%na.omit()
-
+  Birth_wt_df<-birth_wt%>%join(life)%>%join(mum_stat)%>%join(focal_LG_df)%>%na.omit() #joining all relevant tables for analysis
+#setting as factors 
   Birth_wt_df$Sex <- factor(Birth_wt_df$Sex)
   Birth_wt_df$Code <- factor(Birth_wt_df$Code)
   Birth_wt_df$MotherStatus <- factor(Birth_wt_df$MotherStatus)
@@ -120,10 +115,36 @@ for (s in 1:33) {
                              random= ~ vm(Code,ainv)+BirthYear +MumCode, 
                              residual= ~ idv(units),
                              data=Birth_wt_df)
-
-  print(paste0("QGM SUMMARY FOR LG", s))
-  qgm_summary(chr_model_with_ped)
-
+  chr_model_with_ped
 }
+  
+
+all_LG_models<-map(for_models,run_model) # running model for all LG 
+  
+########################################################
+### pulling out effect sizes for birth weight per LG  ###
+##########################################################
+get_sol_focal_LG<-function(model_output){
+
+fixed_eff_table<-as.data.frame(summary(model_output,coef=TRUE)$coef.fixed)
+focal_LG_solution<-fixed_eff_table[6,"solution"] # effect size is the 6th row down in the solutions column 
+focal_LG_solution
+}
+
+effect_sizes<-as.data.frame(map(all_LG_models, get_sol_focal_LG)%>% #getting the effect size for each LG and making table
+  unlist())%>%
+  tibble::rownames_to_column()%>%
+  setNames(c("CHR","effect_size_of_LG"))
+
+
+size_v_length<-join(effect_sizes,deersum)%>%select(-"Max_EstMb")
+
+library(ggplot2)
+ggplot(size_v_length, aes(x=chr_length_KB,y=effect_size_of_LG))+
+  geom_point()+
+  theme_classic()+
+  geom_text(label=size_v_length$CHR, vjust=1.25)+
+  geom_smooth(method=lm)
+  
 
 
