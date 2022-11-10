1cdaa5d3694267744d721f85d2302a7568140f31
annamayh <anna.hewett28@outlook.com>
2022-11-10 15:48:24 +0000

    GWAS plot updated

--- Plots/Pall_GWAS_plot.R
+++ Plots/Pall_GWAS_plot.R
@@ -9,7 +9,7 @@ setwd("M:/")
 
 CMpall<-read.table("PHD_2ndYR/Recombination_ROH/Data_files/Pall_CM_filt_moreSNPs.txt", header = TRUE,stringsAsFactors = FALSE)
 CMpall$CHR<-as.factor(CMpall$CHR)
-#CMpall$CHR<-ordered(CMpall$CHR, levels = c("5", "18", "20", "9","11","12","19","15",
+##CMpall$CHR<-ordered(CMpall$CHR, levels = c("5", "18", "20", "9","11","12","19","15",
  #                                         "30","21","23","1","14","33","25","13","17","29","28", "4",
   #                                        "27","22", "24","8","3","31","6","7","2","16","32","10","26"))
 #no longer need this 
@@ -20,7 +20,7 @@ CMpall$Order <- 1:nrow(CMpall)
 
 axis.set <- CMpall %>% 
   group_by(CHR) %>% 
-  summarize(center = (max(Order) + min(Order)) / 2)
+  dplyr::summarise(center = (max(Order) + min(Order)) / 2)
 
 CMpall$CHR<-as.numeric(CMpall$CHR)
 
@@ -28,7 +28,7 @@ CM_g<-ggplot(CMpall, aes(Order, perc, col = as.factor(CHR%% 2), group=CHR)) +
   scale_colour_manual(values = c("coral", "cornflowerblue")) +
   geom_point() +
   theme_classic()+
-  labs(x="Chromosome",y="ROH density \n(% of ids with a ROH)", title="Using genetic map positions")+
+  labs(x="Chromosome",y="ROH density \n(% of ids with a ROH)", title="Using genetic map (cM) positions")+
   theme(legend.position = "none",
         plot.title = element_text(hjust = 0.5),
         axis.text.x = element_text(size = 8, vjust = 0.5),
@@ -40,7 +40,7 @@ CM_g<-ggplot(CMpall, aes(Order, perc, col = as.factor(CHR%% 2), group=CHR)) +
   geom_line(size=0.8)
 
 ### get location of hotspots 
-mean(CMpall$perc) ##mean pall cm= 0.0577 (11/01/2021) lower than before when had less snps
+mean(CMpall$perc) #
 
 quantile(CMpall$perc, c(.01, .99)) ## 
 length(which(CMpall$perc > 14.86795 )) ##310 hotspot snps
@@ -48,8 +48,14 @@ length(which(CMpall$perc > 14.86795 )) ##310 hotspot snps
 cm_hotspots<-CMpall%>%filter(perc> 14.86795)
 
 
+ggplot(CMpall, aes(CHR, perc))+geom_boxplot()
 
+
+
+
+###########################################################
 ###### BP genomwide plot using same script ################
+###########################################################
 
 BPpall<-read.table("PhD_3rdYR/Data_files/Rum_Pall_BP_filt_UPDATED_SANGER_POS.txt", header = TRUE,stringsAsFactors = FALSE)
 BPpall$CHR<-as.factor(BPpall$CHR)
@@ -64,7 +70,7 @@ BPpall$Order <- 1:nrow(BPpall)
 
 axis.set_BP <- BPpall %>% 
   group_by(CHR) %>% 
-  summarize(center = (max(Order) + min(Order)) / 2)
+  dplyr::summarize(center = (max(Order) + min(Order)) / 2)
 
 BPpall$CHR<-as.numeric(BPpall$CHR)
 
@@ -72,7 +78,7 @@ BP_g<-ggplot(BPpall, aes(Order, perc, col = as.factor(CHR%% 2), group=CHR)) +
   scale_colour_manual(values = c("coral", "cornflowerblue")) +
   geom_point() +
   theme_classic()+
-  labs(tag = "A", y="ROH density \n(%of ids with a ROH)", title = "Using physical map positions")+
+  labs(y="ROH density \n(%of ids with a ROH)", title = "Using physical map (bp) positions")+
   theme(legend.position = "none",
         axis.title.x = element_blank(),
         plot.title = element_text(hjust = 0.5),
@@ -91,11 +97,14 @@ bp_hotspots<-BPpall%>%filter(perc> 15.9197913)
 
 
 A<-BP_g +CM_g + plot_layout(ncol=1)
+A
 
-#ggsave(file="PHD_2ndYR/Manuscript_draft/images/GWAS_Rum_31.png",
- # plot=GWAS_Rum
+ggsave(file="PhD_4th_yr/Heredity_ROH_density_manuscript/Main_images/Rum_pdf3",
+  plot=A,
+  device = "pdf", 
+  dpi=1000
   
-#)
+)
 
 BPpall$BP[BPpall$SNP=="cela1_red_26_30528628"]
 
